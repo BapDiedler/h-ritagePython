acfb7fd464f37e6c25edb1e4b675977de84657f7
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2022-12-22 17:47:26 +0100

    implémentation d'un lion et d'un panda

--- animal/Animal.py
+++ animal/Animal.py
@@ -2,16 +2,42 @@ from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
+
+    @abstractmethod
+    def nom(self):
+        pass
+
+    def onDoitFaire(self,action):
+        print(f"{action} {self.nom()} avec {self.nourrir()}")
+
     @abstractmethod
     def nourrir(self):
         pass
 
+    @abstractmethod
+    def heure(self):
+        pass
+
 
 class Panda(Animal):
+
+    def nom(self):
+        return "le panda"
+
     def nourrir(self):
-        print("nourrir avec du bambou")
+        return "du bambou"
+
+    def heure(self):
+        return "12h/14h"
 
 
-po = Panda()
-po.nourrir()
+class Lion(Animal):
 
+    def nom(self):
+        return "le lion"
+
+    def nourrir(self):
+        return "de la viande"
+
+    def heure(self):
+        return "12h/14h et 19h/20h"
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from animal.Animal import *
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+zoo = [Panda(), Lion()]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for Animal in zoo:
+    Animal.onDoitFaire(action="nourir")
